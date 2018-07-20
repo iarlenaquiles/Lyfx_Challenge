0aab9ca0c8c1cd1ead68126b55ba2e8d748d2e65
Iarlen Aquiles <iarlensilva775@gmail.com>
2018-07-20 12:42:22 -0300

    Criando a página home

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,3 +1,5 @@
+import { DetailsPage } from './../details/details';
+import { AdventureProvider } from './../../providers/adventure/adventure';
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 
@@ -7,8 +9,23 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+  public lista:any = [];
 
+  constructor(public navCtrl: NavController, public adventureProvider: AdventureProvider) { }
+
+
+  ionViewDidLoad() {
+    this.getAdventures();
   }
 
+  getAdventures() {
+    this.adventureProvider.getAdventures().subscribe(
+      data => { this.lista = data; console.log(data);},
+      erro => console.log(erro)
+    );
+  }
+
+  details(adventure) {
+    this.navCtrl.push(DetailsPage, { adventure });
+  }
 }
